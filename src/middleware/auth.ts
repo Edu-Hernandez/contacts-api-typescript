b0977fc6f8bbab62755ea8e9aaf1438c5ab248eb
Alan Edu Hernandez <alaneduh231@gmail.com>
2025-10-23 17:48:27 -0500

    fix: middleware de jwt

---
+++ src/middleware/auth.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { verifyToken } from "../config/jwt";
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return res.status(401).json({ error: "No token provided" });
+
+    const token = authHeader.split(" ")[1];
+    const decoded = verifyToken(token);
+
+    if (!decoded) return res.status(401).json({ error: "Invalid token" });
+
+    req.user = decoded;
+    next();
+}
